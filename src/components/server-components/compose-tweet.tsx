c700e5d8d49b99191a95bf70905a3a2f7d68157b
EmilSunden <emilsunjobb@gmail.com>
2024-05-14 17:45:03 +0200

    feat:adds composeTweet component

---
+++ src/components/server-components/compose-tweet.tsx
@@ -0,0 +1,36 @@
+import { createClient } from "@/utils/supabase/server";
+import { randomUUID } from "crypto";
+import ComposeTweetForm from "../client-components/compose-tweet-form";
+import { revalidatePath } from "next/cache";
+import { db } from "@/lib/db";
+import { tweets } from "@/lib/db/schema";
+
+const ComposeTweet = () => {
+  async function submitTweet(formData: FormData) {
+    "use server";
+    const supabase = createClient();
+    const tweet = formData.get("tweet");
+
+    if (!tweet) return;
+
+    const { data: userData, error: userError } = await supabase.auth.getUser();
+    if (userError) return;
+
+    let err = ""
+
+    const res = await db.insert(tweets).values({
+      userId: userData.user.id,
+      text: tweet.toString(),
+      id: randomUUID(),
+    }).returning().catch((error) => {
+      console.log(error)
+      err = "something wrong with insert"
+    })
+    revalidatePath("/");
+    return { data: res, err};
+  }
+
+  return <ComposeTweetForm serverAction={submitTweet} />;
+};
+
+export default ComposeTweet;
